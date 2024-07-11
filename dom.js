0ea2f7116960565849bfbb06bec2ea61df34a3b7
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2024-07-11 15:13:25 +0300

    111

--- dom.js
+++ dom.js
@@ -1,44 +1,73 @@
 // Создать div
-
+const createDiv = document.createElement("div");
 // Добавить к нему класс wrapper
-
+createDiv.classList.add("wrapper");
 // Поместить его внутрь тэга body
-
+const body = document.body;
+body.appendChild(createDiv);
 // Создать заголовок H1 "DOM (Document Object Model)"
+const header = document.createElement("h1");
+header.textContent = "DOM (Document Object Model)";
 
 // Добавить H1 перед DIV с классом wrapper
-
+createDiv.insertAdjacentElement("beforebegin", header);
 // Создать список <ul></ul>
 // Добавить в него 3 элемента с текстом "один, два, три"
-
+const ul = `
+    <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ul>
+`;
 
 // Поместить список внутрь элемента с классом wrapper
 
+createDiv.innerHTML = ul;
+
 // =================================================
 // Создать изображение
-
+const img = document.createElement("img");
 // Добавить следующие свойства к изображению
 // 1. Добавить атрибут source
-
+img.src = "https://picsum.photos/240";
 // 2. Добавить атрибут width со значением 240
-
+img.width = 240;
 // 3. Добавить класс super
-
+img.classList.add("super");
 // 4. Добавить свойство alt со значением "Super Man"
-
+img.alt = "Super Man";
 // Поместить изображение внутрь элемента с классом wrapper
-
+createDiv.appendChild(img);
 // Используя HTML строку, создать DIV с классом 'pDiv' + c 2-мя параграфами
-
+const elemnHTML = `
+<div class='pDiv'>
+    <p>pharagrah 1</p>
+    <p>pharagrah 2</p>
+</div>`;
 // Поместить этот DIV до элемента <ul></ul>
-
+const ulList = createDiv.querySelector("ul");
+ulList.insertAdjacentHTML("beforebegin", elemnHTML);
 // Добавить для 2-го параграфа класс text
-
+const pDiv = document.querySelector(".pDiv");
+pDiv.children[1].classList.add("text");
 // Удалить 1-й параграф
-
-// Создать функцию generateAutoCard, 
+pDiv.firstElementChild.remove();
+// Создать функцию generateAutoCard,
 // которая принимает 3 аргумента: brand, color, year
-
+const generateAutoCard = (brand, color, year) => {
+  const curDate = new Date();
+  const curYear = curDate.getFullYear();
+  const totalyYear = curYear - year;
+  return `
+    <div class="autoCard">
+        <h2>${brand.toUpperCase()} ${year}</h2>
+        <p>Автомобиль ${brand.toUpperCase()} - ${year} года. Возраст авто - ${totalyYear} лет.</p>
+        <p>Цвет: ${color} </p>
+        <button type='button' class="btn">Удалить</button>
+    </div>
+    `;
+};
 // Функция должна возвращать разметку HTML:
 // <div class="autoCard">
 //   <h2>BRAND YEAR</h2>
@@ -46,21 +75,36 @@
 // </div>
 
 // Создать новый DIV с классом autos
-
+const carsDiv = document.createElement("div");
+carsDiv.classList.add("autos");
 // Создать 3 карточки авто, используя функцию generateAutoCard
 const carsList = [
-    {brand: 'Tesla', year: 2015, color: 'Красный'},
-    {brand: 'Lexus', year: 2016, color: 'Серебристый'},
-    {brand: 'Nissan', year: 2012, color: 'Черный'},
-]
+  { brand: "Tesla", year: 2015, color: "Красный" },
+  { brand: "Lexus", year: 2016, color: "Серебристый" },
+  { brand: "Nissan", year: 2012, color: "Черный" },
+];
+
+const carsHTML = carsList
+  .map((car) => {
+    return generateAutoCard(car.brand, car.color, car.year);
+  })
+  .join("");
 
 // Поместить эти 3 карточки внутрь DIV с классом autos
-
+carsDiv.innerHTML = carsHTML;
 // Поместить DIV c классом autos на страницу DOM - до DIV с классом wrapper
-
+createDiv.insertAdjacentElement("beforebegin", carsDiv);
 // Добавить кнопку Удалить на каждую карточку авто
 
 // При клике на кнопку - удалять карточку из структуры DOM
 // 1. Выбрать все кнопки
+const buttons = document.querySelectorAll(".btn");
 // 2. Создать функцию удаления
+function handleClick(e) {
+  const currentButton = e.currentTarget;
+  currentButton.closest(".autoCard").remove();
+}
 // 3. Использовать цикл - чтобы повесить обработчик события на каждую кнопку
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
